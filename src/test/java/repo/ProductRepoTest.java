fdc55c3b22bf690d8c90c3a5699e3aed47959405
Matthias Hiemer <matthias.hiemer@neuefische.de>
2022-09-12 10:39:36 +0200

    Refactor ProductRepoTest to reduce boiler plate code

--- src/test/java/repo/ProductRepoTest.java
+++ src/test/java/repo/ProductRepoTest.java
@@ -12,33 +12,24 @@ class ProductRepoTest {
     @Test
     void list_whenCalled_returnAllProducts() {
         // GIVEN
-        Product product1 = new Product("A1", "Apfel");
-        Product product2 = new Product("A2", "Banane");
-        Product product3 = new Product("A3", "Zitrone");
-
-        List<Product> productList = List.of(product1, product2, product3);
-
-        ProductRepo productRepo = new ProductRepo(productList);
+        ProductRepo productRepo = generateProductRepo();
 
         // WHEN
         List<Product> actual = productRepo.list();
 
         // THEN
-        assertEquals(productList, actual);
+        List<Product> expected = List.of(
+                new Product("A1", "Apfel"),
+                new Product("A2", "Banane"),
+                new Product("A3", "Zitrone"));
+        assertEquals(expected, actual);
     }
 
     @Test
     void get_whenGivenExistingId_returnProductById(){
 
         // GIVEN
-        // TODO refactor (boiler plate code)
-        Product product1 = new Product("A1", "Apfel");
-        Product product2 = new Product("A2", "Banane");
-        Product product3 = new Product("A3", "Zitrone");
-
-        List<Product> productList = List.of(product1, product2, product3);
-
-        ProductRepo productRepo = new ProductRepo(productList);
+        ProductRepo productRepo = generateProductRepo();
 
         // WHEN
         Product actual = productRepo.get("A2");
@@ -46,5 +37,17 @@ class ProductRepoTest {
         // THEN
         Product expected = new Product("A2", "Banane");
         assertEquals(expected, actual);
+    }
+
+    private static ProductRepo generateProductRepo() {
+        Product product1 = new Product("A1", "Apfel");
+        Product product2 = new Product("A2", "Banane");
+        Product product3 = new Product("A3", "Zitrone");
+
+        List<Product> productList = List.of(product1, product2, product3);
+
+        ProductRepo productRepo = new ProductRepo(productList);
+
+        return productRepo;
     }
 }
